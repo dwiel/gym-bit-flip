9643b0c0c3c7e060dc0270da807432390cb4db0e
Zach Dwiel <zdwiel@gmail.com>
2019-09-05 15:43:02 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 import os
 
-from pip.download import PipSession
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 from setuptools import setup
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
 requirements_file = os.path.join(abs_path, 'requirements.txt')
-install_requirements = parse_requirements(requirements_file, session=PipSession())
+install_requirements = parse_requirements(requirements_file, session=False)
 requirements = [str(ir.req) for ir in install_requirements]
 
 setup(
